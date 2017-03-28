8582a18d5c42f0bcd900600612c1eca82601ec30
dileepsnandanam <dileepsnandanam@gmail.com>
2017-03-28 15:59:51 +0530

    validation for price

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :name, presence: true
+  validates :price, presence: true
   before_save :add_searchable
   belongs_to :shop
   def add_searchable
