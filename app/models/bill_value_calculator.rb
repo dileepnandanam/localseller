5bd19c4c725947763cd82c9935cca0d7d4ba7f32
dileepsnandanam <dileepsnandanam@gmail.com>
2017-03-30 23:35:53 +0530

    fixing bill

--- app/models/bill_value_calculator.rb
+++ app/models/bill_value_calculator.rb
@@ -1,11 +1,11 @@
 class BillValueCalculator
-  def self.calculate(shoping_carts, final_price)
-      price = shoping_carts.map do |cart| 
-        base_price = PurchasePriceCalculator.calculate(cart)
-        base_price = base_price - base_price*2/100 -3 -3 #instamojo takes
-      end.sum
-      price = price - price*2/100 -3 if final_price #instamojo takes on bulk checkout
-      price = 0 if price == -3
-      price
+  def self.calculate(shoping_carts, final_price=false)
+    price = shoping_carts.map do |cart|
+      price = cart.price
+    end.sum
+
+    price = price - price*2/100 -3 if final_price #instamojo takes on bulk checkout
+    price = 0 if price == -3
+    price
   end
 end
--- app/models/purchase_price_calculator.rb
+++ app/models/purchase_price_calculator.rb
@@ -1,11 +1,12 @@
 class PurchasePriceCalculator
-  def self.calculate(shoping_cart)
-    total = shoping_tcart.purchases.map{ |p| p.product.price * p.quantity }.sum,
-    after_instamojo_charges = total + instamojo_charge(total)
-    after_lototribe_charges = after_instamojo_charges + 3
-  end
+	def self.calculate(cart)
+		total = cart.purchases.map{ |purchase| purchase.product.price*purchase.quantity}.sum
+		with_im_charge = total + instamojo_charge(total)
+		with_im_charge
+	end
 
-  def instamojo_charge(price)
-  	price*2/100 + 3
-  end
+	def self.instamojo_charge(price)
+		price*2/100+3
+	end
 end
+
