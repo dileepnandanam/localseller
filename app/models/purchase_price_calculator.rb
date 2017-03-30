434b202e6faa221d826f1914d4a43c431732be80
dileepsnandanam <dileepsnandanam@gmail.com>
2017-03-30 23:41:48 +0530

    forot to take our charges

--- app/models/purchase_price_calculator.rb
+++ app/models/purchase_price_calculator.rb
@@ -2,6 +2,7 @@ class PurchasePriceCalculator
 	def self.calculate(cart)
 		total = cart.purchases.map{ |purchase| purchase.product.price*purchase.quantity}.sum
 		with_im_charge = total + instamojo_charge(total)
+		with_im_charge = with_im_charge + 3
 		with_im_charge
 	end
 
