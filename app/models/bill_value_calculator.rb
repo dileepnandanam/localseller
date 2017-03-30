4a21c4ee0a2fd11c8649067b3984602058f63ce2
dileepsnandanam <dileepsnandanam@gmail.com>
2017-03-30 10:49:41 +0530

    price calculation

--- app/models/bill_value_calculator.rb
+++ app/models/bill_value_calculator.rb
@@ -1,8 +1,11 @@
 class BillValueCalculator
-  def self.calculate(shoping_carts)
-      shoping_carts.map do |cart| 
-        base_price = cart.purchases.map{|p| p.product.price }.sum
-        base_price -3
+  def self.calculate(shoping_carts, final_price)
+      price = shoping_carts.map do |cart| 
+        base_price = cart.purchases.map{|p3| p.product.price }.sum
+        base_price - base_price*2/100 -3 -3
       end.sum
+      price = price - price*2/100 -3 if final_price
+      price = 0 if price == -3
+      price
   end
 end
