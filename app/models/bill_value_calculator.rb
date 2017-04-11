c9feeff2fbaef44da0500050283c6d362771ef76
dileepsnandanam <dileepsnandanam@gmail.com>
2017-04-12 00:23:22 +0530

    fixing bill calculation

--- app/models/bill_value_calculator.rb
+++ app/models/bill_value_calculator.rb
@@ -1,10 +1,8 @@
 class BillValueCalculator
   def self.calculate(shoping_carts, final_price=false)
     price = shoping_carts.map do |cart|
-      cart.price
+      cart.price - cart.price*2/100 -3
     end.sum
-    require 'pry'
-    binding.pry
 
     final_price =  price - price*2/100 - 3 if final_price
     final_price == -3 ? 0 : final_price
