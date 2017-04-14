985a687b44eef64c62695837e217da51ea2a2684
dileepsnandanam <dileepsnandanam@gmail.com>
2017-04-14 09:41:50 +0530

    shoping cart can have product from  multiple shops

--- app/models/bill_value_calculator.rb
+++ app/models/bill_value_calculator.rb
@@ -1,7 +1,7 @@
 class BillValueCalculator
   def self.calculate(purchases, final_price=false)
     price = purchases.map do |purchase|
-      purchase.price - purchase.price*2/100 -3*shoping_cart_fraction(purchase)
+      purchase.price - purchase.price*2/100 -6*shoping_cart_fraction(purchase)
     end.sum
 
 
