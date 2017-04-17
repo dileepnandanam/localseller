f8fc6a69ef8d0194ea816f4f058c622539b52c38
dileepsnandanam <dileepsnandanam@gmail.com>
2017-04-18 02:44:20 +0530

    no need to frequently check location

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+	def time_for_fetch_location
+		if session[:location_fetched_at] && Time.parse(session[:location_fetched_at]) + 24.hours <= Time.now || session[:location_fetched_at].nil?
+			session[:location_fetched_at] = Time.now
+			return true
+		end
+	end
 end
