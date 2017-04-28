ec3828140f4b695277a8769052d5547a49a75da2
dileepsnandanam <dileepsnandanam@gmail.com>
2017-04-29 04:09:49 +0530

    minor corrections

--- app/models/bill_value_calculator.rb
+++ app/models/bill_value_calculator.rb
@@ -12,7 +12,7 @@ class BillValueCalculator
   end
   def self.shoping_cart_fraction(purchase)
     total = purchase.shoping_cart.price
-  	price = purchase.product.price
+  	price = purchase.product.price * purchase.quantity
   	price/total
   end
 end
