56585cefa5760fe1cd15c523171b2db46435f5be
dileepsnandanam <dileepsnandanam@gmail.com>
2017-05-04 20:09:59 +0530

    directory change

--- app/assets/javascripts/shops/comments.js
+++ app/assets/javascripts/shops/comments.js
@@ -2,6 +2,7 @@ $(document).on('turbolinks:load', function(){
 
 	$('#new_comment').on("ajax:success", function(e, data, status, xhr){
 		$('.automated-feedback').html(xhr.responseText)
+		$('#comment_text').val('')
 	})
 
 })
