02852c3988d147b8f47a6916dd9d60a735774b2b
dileepsnandanam <dileepsnandanam@gmail.com>
2017-05-19 14:55:56 +0530

    cosmetic changes

--- app/assets/javascripts/shops/comments.js
+++ app/assets/javascripts/shops/comments.js
@@ -3,11 +3,12 @@ $(document).on('turbolinks:load', function(){
 	$('.new_comment, .reply-form').on("ajax:success", function(e, data, status, xhr){
 		$(this).closest('.comment-form').siblings('.comments-container').prepend(xhr.responseText)
 		$(this).find('#comment_text').val('')
+		$(this).closest('.comment-form').siblings('.comments-container').focus()
 
 	})
 	$('.show-reply-form').on('click', function(e){
 		e.preventDefault();
-		$(this).siblings('.comment-form').toggle()
+		$(this).siblings('.comment-form').toggle(200)
 	})
 
 })
