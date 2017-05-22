8f22c791dd4c372a840b3b6d094504c32c3e119a
dileepsnandanam <dileepsnandanam@gmail.com>
2017-05-22 11:48:52 +0530

    minor changes

--- app/assets/javascripts/shops/comments.js
+++ app/assets/javascripts/shops/comments.js
@@ -3,7 +3,7 @@ $(document).on('turbolinks:load', function(){
 	$('.new_comment, .reply-form').on("ajax:success", function(e, data, status, xhr){
 		$(this).closest('.comment-form').siblings('.comments-container').prepend(xhr.responseText)
 		$(this).find('#comment_text').val('')
-		$(this).closest('.comment-form').siblings('.comments-container').focus()
+		$(this).closest('.comment-form').siblings('.comments-container').find('.new-comment').show(300)
 
 	})
 	$('.show-reply-form').on('click', function(e){
