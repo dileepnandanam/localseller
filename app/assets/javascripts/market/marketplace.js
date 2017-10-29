060a1e33a74655d49250464391f2b2ec042028e7
dileepsnandanam <dileepsnandanam@gmail.com>
2017-10-29 21:07:06 +0530

    search functionality added

--- app/assets/javascripts/market/marketplace.js
+++ app/assets/javascripts/market/marketplace.js
@@ -2,9 +2,38 @@ function init_place_search() {
 	var input = document.getElementById('place')
 	var autocomplete = new google.maps.places.Autocomplete(input, { types: ['geocode'] })
 	$('.search_button').on('click', function(e){
-		e.preventDefault()
 		$('#geolocation').val(autocomplete.getPlace().geometry.location)
-		$(this).closest('form').submit()
 	})
 }
 google.maps.event.addDomListener(window, 'load', init_place_search);
+
+
+				
+var data1={}
+$(document).on('turbolinks:load', function(){
+	$('.search-form').on('ajax:success', function(e, data, status, xhr){
+		$('.results-body > .result-item').remove()
+		$.each(data, function(index, product){
+			$('.results-body').append(
+				`
+				<div class="result-item" data-lat='${product.lat}' data-lng='${product.lng}'>
+				  <div class="result-item-value pull-left">
+				    ${product.name}
+				  </div>
+				  <div class="result-item-value pull-left">
+				    ${product.distance + " Km"} 
+				  </div>
+				  <div class="result-item-value pull-left">
+				    ${product.price}
+				  </div>
+				  <div class="result-item-value pull-left">
+				    ${product.total}
+				  </div>
+				  <div class="clearfix"></div>
+				</div>
+				`
+			)
+
+		})
+	})
+})
