4bc790ebb0f4bbe4457b27e5acba491c92d7ceca
dileepsnandanam <dileepsnandanam@gmail.com>
2017-10-30 14:13:15 +0530

    map marker to products

--- app/assets/javascripts/market/marketplace.js
+++ app/assets/javascripts/market/marketplace.js
@@ -5,16 +5,22 @@ function init_place_search() {
 		$('#geolocation').val(autocomplete.getPlace().geometry.location)
 	})
 }
-google.maps.event.addDomListener(window, 'load', init_place_search);
-
-
-				
-var data1={}
+//google.maps.event.addDomListener(window, 'load', init_place_search);
+$(document).on('turbolinks:load', function(){
+init_place_search()
+})
 $(document).on('turbolinks:load', function(){
 	$('.search-form').on('ajax:success', function(e, data, status, xhr){
+		map_center = $('#geolocation').val().substring(1,$('#geolocation').val().length-1).split(', ')
+		map_center = {lat: parseFloat(map_center[0]), lng: parseFloat(map_center[1])}
+		 var map = new google.maps.Map(document.getElementById('map'), {
+    		zoom: 7,
+    		center: map_center
+  		});
+
 		$('.results-body > .result-item').remove()
 		$.each(data, function(index, product){
-			$('.results-body').append(
+			item = $('.results-body').append(
 				`
 				<div class="result-item" data-lat='${product.lat}' data-lng='${product.lng}'>
 				  <div class="result-item-value pull-left">
@@ -33,6 +39,27 @@ $(document).on('turbolinks:load', function(){
 				</div>
 				`
 			)
+			var marker = new google.maps.Circle({
+            	strokeColor: '#FF0000',
+            	strokeOpacity: 0.8,
+            	strokeWeight: 2,
+            	fillColor: '#FF0000',
+            	fillOpacity: 0.35,
+            	map: map,
+            	center: {lat: product.lat, lng: product.lng},
+            	radius: 10000
+        	});
+
+        	marker.addListener('mouseover', function(){
+        		$(item).children().last().addClass('current')
+        	})
+        	marker.addListener('mouseout', function(){
+        		$(item).children().last().removeClass('current')
+        	})
+
+			
+
+
 
 		})
 	})
