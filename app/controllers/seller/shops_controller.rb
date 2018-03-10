052b33018b9c26824e01813c776bee562f9d9b8e
dileepsnandanam <dileepsnandanam@gmail.com>
2018-03-10 10:36:30 +0530

    fixing bid list

--- app/controllers/seller/shops_controller.rb
+++ app/controllers/seller/shops_controller.rb
@@ -31,13 +31,13 @@ class Seller::ShopsController < SellerController
   end
 
   def open_bids
-    @product_bids = current_user.bids.not_accepted.last_on_top
+    @product_bids = ::Bid.not_accepted.last_on_top.where(product_id: @shop.products.pluck(:id))
     @new_bid_count = @product_bids.count - (Rails.cache.fetch("#{current_user.id}/last_seen_bid_count") || 0)
     Rails.cache.write("#{current_user.id}/last_seen_bid_count", @product_bids.count)
   end
 
   def past_purchases
-    @product_bids = current_user.bids.accepted.last_on_top
+    @product_bids = ::Bid.accepted.last_on_top.where(product_id: @shop.products.pluck(:id))
   end
 
   def edit
--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -1,6 +1,6 @@
 module ShopsHelper
   def new_bids_count
-    product_bids = current_user.bids.not_accepted.last_on_top
+    product_bids = ::Bid.not_accepted.last_on_top.where(product_id: @shop.products.pluck(:id))
     new_bid_count = product_bids.count - (Rails.cache.fetch("#{current_user.id}/last_seen_bid_count") || 0)
     return new_bid_count
   end
